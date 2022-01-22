1b52d92a7c473a6fbb757f19f16d354dd8e4114d
theblessedcity <theblessedcity@gmail.net>
2022-01-23 02:08:31 +0900

    ✨ [Feat] test login oauth kakao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Login from 'Pages/Login';
+import Oauth from 'Pages/Oauth';
+
 const App = () => {
-  return <div className='App'>Hello react</div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/oauth' element={<Oauth />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Pages/Login/View.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const View = ({ loginurl }) => {
+  return (
+    <>
+      <a href={loginurl}>카카오톡 로그인</a>
+    </>
+  );
+};
+
+export default View;
