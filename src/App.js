8a50fd005ed35a666366ef3df10414b0bbed92ff
theblessedcity <theblessedcity@gmail.net>
2022-01-23 23:06:57 +0900

    ✨ [Feat] add route for mypage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Login from 'Pages/Login';
 import Oauth from 'Pages/Oauth';
+import MyPage from 'Pages/MyPage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/oauth' element={<Oauth />} />
+          <Route path='/mypage' element={<MyPage />} />
         </Routes>
       </Router>
     </div>
