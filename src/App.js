7e7c12a0ea47b788e5e3e9bb397044a217cf7bad
theblessedcity <theblessedcity@gmail.net>
2022-01-23 23:31:57 +0900

    ✨ [Feat] add croute /mylike /search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import MyLike from 'Pages/MyLike';
+import Search from 'Pages/Search';
+
 const App = () => {
-  return <div className='App'>Hello react</div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/mylike' element={<MyLike />} />
+          <Route path='/search' element={<Search />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
