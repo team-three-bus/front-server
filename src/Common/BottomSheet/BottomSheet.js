e1b4b8733f9a017d35bd5568cbb5f46c6c0704ce
theblessedcity <theblessedcity@gmail.net>
2022-01-31 18:17:28 +0900

    ✨ [Feat] add component - BottomSheet

---
+++ src/Common/BottomSheet/BottomSheet.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import * as S from './BottomSheet.styles';
+
+import { BottomSheet } from 'react-spring-bottom-sheet';
+import 'react-spring-bottom-sheet/dist/style.css';
+
+export default BottomSheet;
