b5e2f5fe4110bc27860cf4a7b6ccd93ef715e75d
theblessedcity <theblessedcity@gmail.net>
2022-01-31 23:17:20 +0900

    ✨ [Feat] add route for searchresult

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import MyLike from 'Pages/MyLike';
 import Search from 'Pages/Search';
+import SearchResult from 'Pages/SearchResult';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path='/mylike' element={<MyLike />} />
           <Route path='/search' element={<Search />} />
+          <Route path='/searchresult' element={<SearchResult />} />
         </Routes>
       </Router>
     </div>
