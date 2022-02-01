73e0d240af09693b69e2395cf816d3446dbae99d
theblessedcity <theblessedcity@gmail.net>
2022-02-01 22:36:40 +0900

    ✨ [Feat] add main Title

--- src/Common/Title/Title.js
+++ src/Common/Title/Title.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import * as S from './Title.styles';
 
-const Title = ({ title, right, sort }) => {
+const Title = ({ title, subTitle, icon, right, sort }) => {
   return (
-    <S.Wrap>
-      <S.Title sort={sort}>{title}</S.Title>
+    <S.Wrap sort={sort}>
+      <S.TitleBox>
+        <S.Title sort={sort}>
+          {title} {icon && <S.Icon icon={icon} />}
+        </S.Title>
+        {subTitle && <S.SubTitle sort={sort}>{subTitle}</S.SubTitle>}
+      </S.TitleBox>
       {right && <S.Right>{right}</S.Right>}
     </S.Wrap>
   );
--- src/Common/Title/Title.styles.js
+++ src/Common/Title/Title.styles.js
@@ -1,12 +1,31 @@
 import styled from 'styled-components';
 
+import IconLike from './img/icon-like.svg';
+
 export const Wrap = styled.div`
   display: flex;
-  padding-top: 24px;
+
   padding-left: 20px;
   padding-right: 20px;
   justify-content: space-between;
+
+  ${({ sort }) => {
+    switch (sort) {
+      case 'result':
+        return `
+          padding-top: 16px;
+          padding-bottom: 16px;
+        `;
+      case 'main':
+        return `
+          padding-top: 0;
+        `;
+      default:
+        return `padding-top: 24px;`;
+    }
+  }}
 `;
+export const TitleBox = styled.div``;
 
 export const Title = styled.h1`
   color: rgba(239, 88, 106, 1);
@@ -22,14 +41,48 @@ export const Title = styled.h1`
     switch (sort) {
       case 'result':
         return `
-          font-size: 15px;
-          font-weight: 400;
-          color: rgba(159, 159, 159, 1);
-          line-height :17.02px;
+          font-family: NanumSquareRound;
+          font-size: 12px;
+          font-weight: 700;
+          text-align: left;
+          color: #000000;;
+        `;
+      case 'main':
+        return `
+          font-family: NanumSquareRound;
+          font-size: 20px;
+          font-weight: 800;
+          line-height: 24px;
+          text-align: left;
+          color: #000000;
+          margin: 0;
+          padding-top: 6px;
+          padding-bottom: 6px;
         `;
     }
     return ``;
   }}
 `;
 
+export const Icon = styled.span`
+  display: inline-block;
+  width: 48px;
+  height: 48px;
+  vertical-align: bottom;
+  ${({ icon }) => {
+    switch (icon) {
+      case 'like':
+        return `background: url(${IconLike}) center center no-repeat;`;
+    }
+  }}
+`;
+export const SubTitle = styled.h2`
+  font-family: NanumSquareRound;
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 17px;
+  text-align: left;
+  color: #9f9f9f;
+  margin: 0;
+`;
 export const Right = styled.div``;
