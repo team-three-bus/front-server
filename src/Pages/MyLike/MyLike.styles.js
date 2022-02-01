9403386f2daf33eb991657284aacd337ef67b698
theblessedcity <theblessedcity@gmail.net>
2022-02-01 22:45:55 +0900

    ✨ [Feat] update mylike

--- src/Pages/MyLike/MyLike.styles.js
+++ src/Pages/MyLike/MyLike.styles.js
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+
+export const FilterBox = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  border-bottom: 10px solid #f8f9fa;
+
+  & > span {
+    margin-right: 8px;
+  }
+`;
--- src/Pages/MyLike/View.js
+++ src/Pages/MyLike/View.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import * as S from './MyLike.styles';
 
+import { Layout } from 'Common/Layout';
+import { Title } from 'Common/Title';
+import { Choice } from 'Common/Choice';
 import { Checkbox } from 'Common/Form';
 import { Product } from 'Common/Product';
 import tempProductImg from 'Common/Product/img/tempProductImg.jpg';
@@ -9,16 +12,32 @@ import { ItemList, Item } from 'Common/ItemList';
 
 const View = ({ isEvent, setIsEvent }) => {
   return (
-    <>
-      <Checkbox
-        id='isEvent'
-        name='isEvent'
-        label={`행사 중 상품만 보기`}
-        checked={isEvent}
-        onChange={(checked) => {
-          setIsEvent(checked);
-        }}
+    <Layout header='sub' title='완내스'>
+      <Title
+        sort='main'
+        icon='like'
+        title='완전 내 스타일!'
+        subTitle='찜한 상품을 모아서 볼 수 있어요.'
       />
+      <S.FilterBox>
+        <Choice isChoosed={false}>편의점</Choice>
+        <Choice isChoosed={true}>카테고리 2</Choice>
+      </S.FilterBox>
+      <Title
+        sort={'result'}
+        right={
+          <Checkbox
+            id='isEvent'
+            name='isEvent'
+            label={`행사 중 상품만 보기`}
+            checked={isEvent}
+            onChange={(checked) => {
+              setIsEvent(checked);
+            }}
+          />
+        }
+      />
+
       {
         <ItemList>
           <Item>
@@ -68,7 +87,7 @@ const View = ({ isEvent, setIsEvent }) => {
           </Item>
         </ItemList>
       }
-    </>
+    </Layout>
   );
 };
 
