4603089dcb1a132931b6f3cacf3643b3b626f7e1
rarlala <wtw9611@gmail.com>
2022-02-11 02:04:41 +0900

    ✨ [Feat] Add category Link

--- src/Common/Category/Constant.js
+++ src/Common/Category/Constant.js
@@ -10,34 +10,42 @@ import item08 from "./img/item08.svg";
 export const CATEGORY_LIST = [
   {
     img : item01,
-    name: "생활용품"
+    name: "생활용품",
+    link : '/category?category=생활용품'
   },
   {
     img : item02,
-    name: "아이스크림"
+    name: "아이스크림",
+    link : '/category?category=아이스크림'
   },
   {
     img : item03,
-    name: "탄산음료"
+    name: "탄산음료",
+    link : '/category?category=탄산음료'
   },
   {
     img : item04,
-    name: "유제품/커피 음료"
+    name: "유제품/커피 음료",
+    link : '/category?category=유제품/커피 음료'
   },
   {
     img : item05,
-    name: "생수/과채/기타음료"
+    name: "생수/과채/기타음료",
+    link : '/category?category=생수/과채/기타음료'
   },
   {
     img : item06,
-    name: "간편식사"
+    name: "간편식사",
+    link : '/category?category=간편식사'
   },
   {
     img : item07,
-    name: "빵/과자류"
+    name: "빵/과자류",
+    link : '/category?category=빵/과자류'
   },
   {
     img : item08,
-    name: "기타"
+    name: "기타",
+    link : '/category?category=기타'
   }
 ]
