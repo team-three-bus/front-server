7c804e208528373f71a896f646e97a5829621c92
theblessedcity <theblessedcity@gmail.net>
2022-02-13 16:28:02 +0900

    ✨  [Style] apply design to Checkbox

--- src/Common/Form/Checkbox/Checkbox.styles.js
+++ src/Common/Form/Checkbox/Checkbox.styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import IconCheck from './img/icon-check.svg';
+
 export const UICheckbox = styled.div`
   display: inline-block;
   vertical-align: middle;
@@ -8,16 +10,34 @@ export const UICheckbox = styled.div`
 `;
 
 export const Checkbox = styled.input`
+  position: relative;
+  display: inline-block;
+  width: 18px;
+  height: 18px;
+  margin: 0;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 4px;
+  box-sizing: border-box;
   vertical-align: middle;
+  background-color: #f8f9fa;
+  appearance: none;
+  ${({ checked }) => {
+    return checked
+      ? `
+        background: #EF586A url(${IconCheck}) center center no-repeat;
+        border: 1px solid #EF586A;
+      `
+      : '';
+  }}
 `;
 
 export const Label = styled.label`
-  font-family: NanumSquareRound;
-  font-size: 14px;
+  font-family: NanumSquare;
+  font-size: 12px;
   font-weight: 700;
-  line-height: 16px;
-  text-align: left;
-  color: #212529;
+  line-height: 14px;
+  text-align: right;
+  color: #000000;
   margin-left: 9px;
   vertical-align: middle;
 `;
