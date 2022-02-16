88381f1da4a843cd67fa2ee7bc7b6e56d4811069
rarlala <wtw9611@gmail.com>
2022-02-16 22:26:43 +0900

    💄 [Style] mypage UI

--- src/Pages/MyPage/View.js
+++ src/Pages/MyPage/View.js
@@ -9,14 +9,19 @@ import listIcon from "./img/ic-list.svg";
 import mailIcon from "./img/ic-mail.svg";
 
 const MyPage = styled.div`
+  box-sizing: border-box;
+  max-width: 768px;
+  min-height: calc(100vh - 64px);
+  margin: 0 auto;
   padding: 0 20px;
+  background: #fff;
 `
 
 const MyPageTop = styled.div`
   display: flex;
   justify-content: space-between;
   align-item: center;
-  margin: 26px 0 55px;
+  padding: 26px 0 55px;
 `
 
 const MyPageBottom = styled.div`
