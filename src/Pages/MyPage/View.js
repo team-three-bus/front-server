d75a9db535c0da2b20070b8f7f2dd05d21c55557
rarlala <wtw9611@gmail.com>
2022-02-16 12:00:00 +0900

    💄 [Style] MyPage UI

--- src/Pages/MyPage/View.js
+++ src/Pages/MyPage/View.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Layout } from 'Common/Layout';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+import settingIcon from "./img/ic-setting.svg";
+import heartIcon from "./img/ic-heart.svg";
+import listIcon from "./img/ic-list.svg";
+import mailIcon from "./img/ic-mail.svg";
 
 const MyPage = styled.div`
   padding: 0 20px;
@@ -11,6 +17,34 @@ const MyPageTop = styled.div`
   justify-content: space-between;
   align-item: center;
   margin: 26px 0 55px;
+`
+
+const MyPageBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const MyPageButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  width: 100%;
+  height: 56px;
+  margin-bottom: 8px;
+  padding-left: 25px;
+  background: #F1F3F5;
+  border-radius: 40px;
+  border: none;
+  outline: none;
+  font-weight: bold;
+  font-size: 14px;
+  text-align: left;
+  font-family: 'Apple SD Gothic Neo', sans-serif;
+  color: #212529;
+  text-decoration: none;
+  img {
+    margin-right: 14px;
+  }
 `
 
 const Nickname = styled.p`
@@ -41,6 +75,13 @@ const View = ({ nickname, onClickLogout }) => {
         <Nickname><strong>{nickname}</strong> 님</Nickname> 
         <Logout onClick={onClickLogout}>로그아웃</Logout>
       </MyPageTop>
+      <MyPageBottom>
+        {/* TODO : 맞는 링크로 이동 처리 */}
+        <MyPageButton to="/"><img src={settingIcon}/>정보 설정</MyPageButton>
+        <MyPageButton to="/mylike"><img src={heartIcon}/>완.내.스!</MyPageButton>
+        <MyPageButton to="/"><img src={listIcon}/>서비스 소개</MyPageButton>
+        <MyPageButton to="/"><img src={mailIcon}/>문의하기</MyPageButton>
+      </MyPageBottom>
     </MyPage>
     </>
   );
