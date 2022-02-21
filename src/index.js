53d0aa59e21f12a947f663435e44e2ad253d17e9
rarlala <wtw9611@gmail.com>
2022-02-22 00:09:08 +0900

    ✨ [Feat] react ga

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-221114908-1");
 
 ReactDOM.render(
   <React.StrictMode>
