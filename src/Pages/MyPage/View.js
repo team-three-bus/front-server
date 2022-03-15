fcdfdd75e88f02994125e9ba0ac13faba87e86c5
rarlala <wtw9611@gmail.com>
2022-03-16 01:05:39 +0900

    🐛 [Fix] footer style issue

--- src/Pages/MyPage/View.js
+++ src/Pages/MyPage/View.js
@@ -5,13 +5,12 @@ import styled from 'styled-components';
 
 import settingIcon from './img/ic-setting.svg';
 import heartIcon from './img/ic-heart.svg';
-import listIcon from './img/ic-list.svg';
+// import listIcon from './img/ic-list.svg';
 import mailIcon from './img/ic-mail.svg';
 
 const MyPage = styled.div`
   box-sizing: border-box;
   max-width: 768px;
-  min-height: calc(100vh - 64px);
   margin: 0 auto;
   padding: 0 20px;
   background: #fff;
@@ -74,39 +73,40 @@ const Logout = styled.p`
 const View = ({ nickname, onClickLogout, mailto }) => {
   return (
     <>
-      <Layout header='sub' title='마이페이지' />
-      <MyPage>
-        <MyPageTop>
-          <Nickname>
-            <strong>{nickname}</strong> 님
-          </Nickname>
-          <Logout onClick={onClickLogout}>로그아웃</Logout>
-        </MyPageTop>
-        <MyPageBottom>
-          {/* TODO : 맞는 링크로 이동 처리 */}
-          <MyPageButton to='/mysetting'>
-            <img src={settingIcon} />
-            정보 설정
-          </MyPageButton>
-          <MyPageButton to='/mylike'>
-            <img src={heartIcon} />
-            완.내.스!
-          </MyPageButton>
-          {/* {<MyPageButton to='/'>
-            <img src={listIcon} />
-            서비스 소개
-          </MyPageButton>} */}
-          <MyPageButton
-            to='#'
-            onClick={() => {
-              window.location.href = mailto;
-            }}
-          >
-            <img src={mailIcon} />
-            문의하기
-          </MyPageButton>
-        </MyPageBottom>
-      </MyPage>
+      <Layout header='sub' title='마이페이지'>
+        <MyPage>
+          <MyPageTop>
+            <Nickname>
+              <strong>{nickname}</strong> 님
+            </Nickname>
+            <Logout onClick={onClickLogout}>로그아웃</Logout>
+          </MyPageTop>
+          <MyPageBottom>
+            {/* TODO : 맞는 링크로 이동 처리 */}
+            <MyPageButton to='/mysetting'>
+              <img src={settingIcon} />
+              정보 설정
+            </MyPageButton>
+            <MyPageButton to='/mylike'>
+              <img src={heartIcon} />
+              완.내.스!
+            </MyPageButton>
+            {/* {<MyPageButton to='/'>
+              <img src={listIcon} />
+              서비스 소개
+            </MyPageButton>} */}
+            <MyPageButton
+              to='#'
+              onClick={() => {
+                window.location.href = mailto;
+              }}
+            >
+              <img src={mailIcon} />
+              문의하기
+            </MyPageButton>
+          </MyPageBottom>
+        </MyPage>
+      </Layout>
     </>
   );
 };
