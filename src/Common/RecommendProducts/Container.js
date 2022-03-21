83e6fd75c61bc5994b1ef23cf3b53fc6f0b6a8ec
theblessedcity <theblessedcity@gmail.net>
2022-03-22 00:29:01 +0900

    [fix] add like func to RecommandProducts

--- src/Common/RecommendProducts/Container.js
+++ src/Common/RecommendProducts/Container.js
@@ -1,37 +1,61 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import * as request from 'Common/Util/HttpRequest';
-import View from "./View";
+import View from './View';
 
 function Container() {
-  const [nickname, setNickname] = useState(localStorage.getItem('nickname') !== 'undefined' && localStorage.getItem('nickname'));
+  const navigate = useNavigate();
+  const [nickname, setNickname] = useState(
+    localStorage.getItem('nickname') !== 'undefined' &&
+      localStorage.getItem('nickname')
+  );
   const [recommendItems, setRecommendItems] = useState([]);
 
   useEffect(() => {
     getData();
     getNickName();
-  }, [])
+  }, []);
 
   const getData = async () => {
-    await request.getRecommendItems()
-      .then(res => setRecommendItems(res.data.list))
-  }
+    await request
+      .getRecommendItems()
+      .then((res) => setRecommendItems(res.data.list));
+  };
 
   const getNickName = async () => {
-   await request.getNickname()
-    .then(res => setNickname(res.data.nickname))
-  }
+    await request.getNickname().then((res) => setNickname(res.data.nickname));
+  };
 
   const gotoDetail = (id) => {
     return (window.location.href = `/detail/${id}`);
   };
 
+  const changeLike = async (product) => {
+    const { id, isLike } = product;
+    const token = localStorage.getItem('access_token');
+    if (!token) return navigate('/login');
+    if (isLike) {
+      await request.deleteLikeItem({ id });
+    } else {
+      await request.postLikeItem({ id });
+    }
+    const newRecommendItems = recommendItems.map((product) => {
+      if (product.id === id) {
+        product.isLike = !product.isLike;
+      }
+      return product;
+    });
+    setRecommendItems(newRecommendItems);
+  };
+
   return (
-    <View 
+    <View
       nickname={nickname}
       recommendItems={recommendItems}
       gotoDetail={gotoDetail}
+      changeLike={changeLike}
     />
-  )
+  );
 }
 
 export default Container;
--- src/Common/RecommendProducts/View.js
+++ src/Common/RecommendProducts/View.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 import { Title } from 'Common/Title';
 import { Product } from 'Common/Product';
 import tempProductImg from 'Common/Product/img/tempProductImg.jpg';
@@ -9,7 +9,7 @@ const RecommendProductBox = styled.div`
   margin-top: 10px;
   padding: 7px 0 3px;
   background: #ffffff;
-`
+`;
 
 const RecommendItemList = styled.div`
   display: flex;
@@ -20,7 +20,7 @@ const RecommendItemList = styled.div`
     min-width: 135px;
   }
   > div + div {
-    margin-left: 16px
+    margin-left: 16px;
   }
   &::-webkit-scrollbar {
     visibility: hidden;
@@ -28,44 +28,41 @@ const RecommendItemList = styled.div`
   &::-webkit-scrollbar-thumb {
     background: transparent;
   }
-`
+`;
 
-const View = ({
-  recommendItems,
-  nickname,
-  gotoDetail
-}) => {
+const View = ({ recommendItems, nickname, gotoDetail, changeLike }) => {
   return (
     <RecommendProductBox>
       <Title
-        sort="main"
-        icon="recommend"
+        sort='main'
+        icon='recommend'
         subTitle={
-          nickname ? 
-            `${nickname}님을 위해 편하가 준비한 추천 상품!` : 
-            "가입/로그인 하시면 더 정확한 추천을 받으실 수 있어요!"
+          nickname
+            ? `${nickname}님을 위해 편하가 준비한 추천 상품!`
+            : '가입/로그인 하시면 더 정확한 추천을 받으실 수 있어요!'
         }
-        title="추천상품"
+        title='추천상품'
       />
       <RecommendItemList>
-        {recommendItems.length === 0 && "Loading..."}
+        {recommendItems.length === 0 && 'Loading...'}
         {recommendItems.map((item, i) => (
           <Product
             key={i}
             id={item.id}
             img={item.imageurl}
             price={item.price}
+            like={item.isLike}
             plus={item.eventtype}
             title={item.name}
             store={item.brand}
             saleend={!item.isevent}
             gotoDetail={() => gotoDetail(item.id)}
-            // like={}
+            changeLike={() => changeLike(item)}
           />
         ))}
       </RecommendItemList>
     </RecommendProductBox>
   );
-}
+};
 
 export default View;
